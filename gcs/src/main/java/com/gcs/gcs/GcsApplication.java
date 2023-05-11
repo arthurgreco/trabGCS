c6cc33d1efd406b61062694ecf38ea769af8db7e
Arthur Greco <arthurgreco9@gmail.com>
2023-05-11 14:34:52 -0300

    ex3

--- gcs/src/main/java/com/gcs/gcs/GcsApplication.java
+++ gcs/src/main/java/com/gcs/gcs/GcsApplication.java
@@ -1,4 +1,6 @@
 package com.gcs.gcs;
+import java.io.IOException;
+import java.util.Scanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +10,20 @@ public class GcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcsApplication.class, args);
+
+
+        // 1016
+        Scanner in = new Scanner(System.in);
+        int km = in.nextInt();
+        int minutos = (60 * km) / 30;
+        System.out.println(minutos + " minutos");
+
+
+        // 1004
+        int A, B;
+        A = in.nextInt();
+        B = in.nextInt();
+        System.out.println("PROD = " + (A * B));
 	}
 
 }
